66ecfe47a8bd852e390ac0fa45554c464f0c0320
Peter Christensen <pch@ordbogen.com>
2013-12-12 22:18:34 +0100

    Added connection code

--- proto/agentx/agentx.c
+++ proto/agentx/agentx.c
@@ -45,18 +45,59 @@ static struct proto *agentx_init(struct proto_config *C)
 	return &p->p;
 }
 
+static void agentx_connected_hook(sock *sk UNUSED)
+{
+	/* TODO - Send open and set AGENTX_STATE_OPEN_SENT */
+}
+
+static void agentx_err_hook(sock *sk UNUSED, int er UNUSED)
+{
+}
+
+static int agentx_connect(struct agentx_proto *p)
+{
+	sock *sk;
+
+	sk = sk_new(p->p.pool);
+	if (p->cf->transport == AGENTX_TRANSPORT_TCP)
+	{
+		sk->type = SK_TCP_ACTIVE;
+		sk->data = p;
+		sk->saddr = IPA_NONE;
+		sk->daddr = p->cf->agent.tcp.addr;
+		sk->dport = p->cf->agent.tcp.port;
+	}
+	sk->tx_hook = agentx_connected_hook;
+	sk->err_hook = agentx_err_hook;
+
+	p->state = AGENTX_STATE_CONNECTING;
+	p->sk = sk;
+	if (sk_open(sk) < 0)
+	{
+		p->state = AGENTX_STATE_DISCONNECTED;
+		return 0;
+	}
+
+	return 1;
+}
+
 static int agentx_start(struct proto *P)
 {
 	struct agentx_proto *p = (struct agentx_proto *)P;
 
 	snmp_add_protocol(&p->snmp);
 
-	return PS_START;
+	if (agentx_connect(p))
+		return PS_START;
+	else
+		return PS_DOWN;
 }
 
 static int agentx_shutdown(struct proto *P)
 {
 	struct agentx_proto *p = (struct agentx_proto *)P;
+
+	/* TODO */
 
 	snmp_remove_protocol(&p->snmp);
 
