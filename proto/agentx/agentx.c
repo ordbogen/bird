5bb7560b47eb3d0854c0547ac17bfa1d8b32ae30
Peter Christensen <pch@ordbogen.com>
2014-03-07 21:06:34 +0100

    Reindented as per BIRD coding style

--- proto/agentx/agentx.c
+++ proto/agentx/agentx.c
@@ -2,52 +2,52 @@
 
 static void agentx_register_hook(snmp_protocol *snmp, const snmp_registration *registration)
 {
-	struct agentx_proto *p = (struct agentx_proto *)snmp->user_data;
-	if (p->state == AGENTX_STATE_ESTABLISHED)
-	{
-		/* TODO */
-	}
+  struct agentx_proto *p = (struct agentx_proto *)snmp->user_data;
+  if (p->state == AGENTX_STATE_ESTABLISHED)
+  {
+    /* TODO */
+  }
 }
 
 static void agentx_unregister_hook(snmp_protocol *snmp, const snmp_registration *registration)
 {
-	struct agentx_proto *p = (struct agentx_proto *)snmp->user_data;
-	if (p->state == AGENTX_STATE_ESTABLISHED)
-	{
-		/* TODO */
-	}
+  struct agentx_proto *p = (struct agentx_proto *)snmp->user_data;
+  if (p->state == AGENTX_STATE_ESTABLISHED)
+  {
+    /* TODO */
+  }
 }
 
 static void agentx_notify_hook(snmp_protocol *snmp, const u32 *oid, unsigned int oid_size, const list *varbinds)
 {
-	struct agentx_proto *p = (struct agentx_proto *)snmp->user_data;
-	if (p->state == AGENTX_STATE_ESTABLISHED)
-	{
-		/* TODO */
-	}
+  struct agentx_proto *p = (struct agentx_proto *)snmp->user_data;
+  if (p->state == AGENTX_STATE_ESTABLISHED)
+  {
+    /* TODO */
+  }
 }
 
 static struct proto *agentx_init(struct proto_config *C)
 {
-	struct agentx_proto *p = (struct agentx_proto *)proto_new(C, sizeof(*p));
+  struct agentx_proto *p = (struct agentx_proto *)proto_new(C, sizeof(*p));
 
-	p->cf = (struct agentx_config *)C;
+  p->cf = (struct agentx_config *)C;
 
-	p->snmp.register_hook = agentx_register_hook;
-	p->snmp.unregister_hook = agentx_unregister_hook;
-	p->snmp.notify_hook = agentx_notify_hook;
-	p->snmp.user_data = (void *)p;
+  p->snmp.register_hook = agentx_register_hook;
+  p->snmp.unregister_hook = agentx_unregister_hook;
+  p->snmp.notify_hook = agentx_notify_hook;
+  p->snmp.user_data = (void *)p;
 
-	p->state = AGENTX_STATE_DISABLED;
-	p->sk = NULL;
-	p->session_id = 0;
+  p->state = AGENTX_STATE_DISABLED;
+  p->sk = NULL;
+  p->session_id = 0;
 
-	return &p->p;
+  return &p->p;
 }
 
 static void agentx_connected_hook(sock *sk UNUSED)
 {
-	/* TODO - Send open and set AGENTX_STATE_OPEN_SENT */
+  /* TODO - Send open and set AGENTX_STATE_OPEN_SENT */
 }
 
 static void agentx_err_hook(sock *sk UNUSED, int er UNUSED)
@@ -56,58 +56,58 @@ static void agentx_err_hook(sock *sk UNUSED, int er UNUSED)
 
 static int agentx_connect(struct agentx_proto *p)
 {
-	sock *sk;
+  sock *sk;
 
-	sk = sk_new(p->p.pool);
-	if (p->cf->transport == AGENTX_TRANSPORT_TCP)
-	{
-		sk->type = SK_TCP_ACTIVE;
-		sk->data = p;
-		sk->saddr = IPA_NONE;
-		sk->daddr = p->cf->agent.tcp.addr;
-		sk->dport = p->cf->agent.tcp.port;
-	}
-	sk->tx_hook = agentx_connected_hook;
-	sk->err_hook = agentx_err_hook;
+  sk = sk_new(p->p.pool);
+  if (p->cf->transport == AGENTX_TRANSPORT_TCP)
+  {
+    sk->type = SK_TCP_ACTIVE;
+    sk->data = p;
+    sk->saddr = IPA_NONE;
+    sk->daddr = p->cf->agent.tcp.addr;
+    sk->dport = p->cf->agent.tcp.port;
+  }
+  sk->tx_hook = agentx_connected_hook;
+  sk->err_hook = agentx_err_hook;
 
-	p->state = AGENTX_STATE_CONNECTING;
-	p->sk = sk;
-	if (sk_open(sk) < 0)
-	{
-		p->state = AGENTX_STATE_DISCONNECTED;
-		return 0;
-	}
+  p->state = AGENTX_STATE_CONNECTING;
+  p->sk = sk;
+  if (sk_open(sk) < 0)
+  {
+    p->state = AGENTX_STATE_DISCONNECTED;
+    return 0;
+  }
 
-	return 1;
+  return 1;
 }
 
 static int agentx_start(struct proto *P)
 {
-	struct agentx_proto *p = (struct agentx_proto *)P;
+  struct agentx_proto *p = (struct agentx_proto *)P;
 
-	snmp_add_protocol(&p->snmp);
+  snmp_add_protocol(&p->snmp);
 
-	if (agentx_connect(p))
-		return PS_START;
-	else
-		return PS_DOWN;
+  if (agentx_connect(p))
+    return PS_START;
+  else
+    return PS_DOWN;
 }
 
 static int agentx_shutdown(struct proto *P)
 {
-	struct agentx_proto *p = (struct agentx_proto *)P;
+  struct agentx_proto *p = (struct agentx_proto *)P;
 
-	/* TODO */
+  /* TODO */
 
-	snmp_remove_protocol(&p->snmp);
+  snmp_remove_protocol(&p->snmp);
 
-	return PS_DOWN;
+  return PS_DOWN;
 }
 
 struct protocol proto_agentx = {
-	.name =				"AgentX",
-	.template =			"agentx%d",
-	.init =				agentx_init,
-	.start =			agentx_start,
-	.shutdown =			agentx_shutdown,
+  .name =			"AgentX",
+  .template =			"agentx%d",
+  .init =			agentx_init,
+  .start =			agentx_start,
+  .shutdown =			agentx_shutdown,
 };
--- proto/agentx/agentx.h
+++ proto/agentx/agentx.h
@@ -14,54 +14,54 @@
 
 struct agentx_tcp_agent
 {
-	ip_addr addr;
-	int port;
+  ip_addr addr;
+  int port;
 };
 
 struct agentx_unix_agent
 {
-	char path[108];
+  char path[108];
 };
 
 typedef union agentx_agent
 {
-	struct agentx_tcp_agent tcp;
+  struct agentx_tcp_agent tcp;
 } agentx_agent;
 
 typedef enum _agentx_transport
 {
-	AGENTX_TRANSPORT_NONE,
-	AGENTX_TRANSPORT_TCP,
-/*	AGENTX_TRANSPORT_UNIX*/
+  AGENTX_TRANSPORT_NONE,
+  AGENTX_TRANSPORT_TCP,
+  /* AGENTX_TRANSPORT_UNIX */
 } agentx_transport;
 
 struct agentx_config
 {
-	struct proto_config c;
-	agentx_transport transport;
-	agentx_agent agent;
-	int timeout;
+  struct proto_config c;
+  agentx_transport transport;
+  agentx_agent agent;
+  int timeout;
 };
 
 typedef enum _agentx_state
 {
-	AGENTX_STATE_DISABLED,
-	AGENTX_STATE_DISCONNECTED,
-	AGENTX_STATE_CONNECTING,
-	AGENTX_STATE_OPEN_SENT,
-	AGENTX_STATE_ESTABLISHED
+  AGENTX_STATE_DISABLED,
+  AGENTX_STATE_DISCONNECTED,
+  AGENTX_STATE_CONNECTING,
+  AGENTX_STATE_OPEN_SENT,
+  AGENTX_STATE_ESTABLISHED
 } agentx_state;
 
 struct agentx_proto
 {
-	struct proto p;
-	struct agentx_config *cf;
+  struct proto p;
+  struct agentx_config *cf;
 
-	snmp_protocol snmp;
+  snmp_protocol snmp;
 
-	agentx_state state;
-	sock *sk; /* Valid in CONNECTING, OPEN_SENT, and ESTABLISHED states */
-	u32 session_id; /* Only valid in the ESTABLISHED state */
+  agentx_state state;
+  sock *sk; /* Valid in CONNECTING, OPEN_SENT, and ESTABLISHED states */
+  u32 session_id; /* Only valid in the ESTABLISHED state */
 };
 
 #endif // _BIRD_AGENTX_H_
