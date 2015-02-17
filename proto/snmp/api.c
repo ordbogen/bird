e0db03b65d5699d82b567d15eaecb34155e8c71c
Peter Christensen <pch@ordbogen.com>
2015-02-17 21:28:50 +0100

    Added api.c

---
+++ proto/snmp/api.c
@@ -0,0 +1,14 @@
+#include "snmp.h"
+
+void snmp_send_notification(const snmp_object_identifier *notification, ...)
+{
+	if (snmp_instance == NULL)
+		return;
+
+	va_list args;
+	va_start(args, notification);
+	snmp_enqueue_notificationv(snmp_instance, notification, args);
+	va_end(args);
+}
+
+
