0851b6e6a71788d9bff5d5ff256a59b37ccd4e48
Peter Christensen <pch@ordbogen.com>
2015-02-17 21:50:02 +0100

    Fixed indentation

--- nest/snmp.c
+++ nest/snmp.c
@@ -10,13 +10,13 @@
 
 void snmp_send_notification(const snmp_object_identifier *notification, ...)
 {
-	if (snmp_instance == NULL)
-		return;
+  if (snmp_instance == NULL)
+    return;
 
-	va_list args;
-	va_start(args, notification);
-	snmp_enqueue_notificationv(snmp_instance, notification, args);
-	va_end(args);
+  va_list args;
+  va_start(args, notification);
+  snmp_enqueue_notificationv(snmp_instance, notification, args);
+  va_end(args);
 }
 
 #endif /* CONFIG_SNMP */
