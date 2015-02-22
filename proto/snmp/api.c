33787f03bb93e06148351feee16ca93dd7d0d75b
Peter Christensen <pch@ordbogen.com>
2015-02-22 21:40:38 +0100

    Old fashioned C style

--- proto/snmp/api.c
+++ proto/snmp/api.c
@@ -32,10 +32,10 @@
  */
 void snmp_send_notification(const snmp_object_identifier *notification, ...)
 {
+  va_list args;
   if (snmp_instance == NULL)
     return;
 
-  va_list args;
   va_start(args, notification);
   snmp_enqueue_notificationv(snmp_instance, notification, args);
   va_end(args);
