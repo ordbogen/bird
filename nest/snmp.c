d557c5347ff45f16a74dbed83a753a06893e1d34
Peter Christensen <pch@ordbogen.com>
2015-03-16 20:30:54 +0100

    Added documentation to snmp_send_notification

--- nest/snmp.c
+++ nest/snmp.c
@@ -8,6 +8,36 @@
 
 #include <stdarg.h>
 
+/**
+ * snmp_send_notification - Send SNMP notification
+ * @notification: Notification identifier
+ *
+ * Generates an SNMP notification with a series of SNMP values.
+ *
+ * You specify the values in name-type-value pairs. The first two
+ * arguments are const snmp_object_identifier *, and snmp_value_type
+ * respectively. The type of the third argument depends on the
+ * snmp_value_type.
+ *
+ * Octet strings has a fouth argument with the size of the string.
+ * -1 denotes zero-terminated string.
+ *
+ * The types (and their respective arguments) are:
+ *
+ * SNMP_INTEGER (int)
+ *
+ * SNMP_OCTET_STRING (const void *, int)
+ *
+ * SNMP_OCTET_IDENTIFIER (const snmp_object_identifier *)
+ *
+ * SNMP_IP_ADDRESS (const struct ip_addr *)
+ *
+ * SNMP_COUNTER32 (unsigned int)
+ *
+ * SNMP_UNSIGNED32 (unsigned int)
+ *
+ * SNMP_TIME_TICKS (unsigned int)
+ */
 void snmp_send_notification(const snmp_object_identifier *notification, ...)
 {
   if (snmp_instance == NULL)
